74c6023fd08cecc4be1cd29317b0de15f121772c
ogwalu chidozie <ogwaluchidozie@gmail.com>
2023-05-19 11:28:15 +0100

    schema model for the application

---
+++ src/model/usermodel.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        firstname: {
+            type: String,
+            required: [true, "Firstname is needed"]
+        },
+        lastname: {
+            type: String,
+            required: [true, "Lastname is needed"]
+        },
+        profilephoto: {
+            type: String,
+        },
+        email: {
+            type: String,
+            required: [true, "Firstname is needed"]
+        },
+        password: {
+            type: String,
+            required: [true, "password is needed"]
+        },
+        confim:{
+            type:String,
+            required:[true, "confirm password"]
+        },
+        isBlocked: {
+            type: Boolean,
+            default: false
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false
+        },
+        role: {
+            type: String,
+            enum: ["Admin", "Client", "Guest"]
+        },
+        
+    },
+    {
+        timestamps: true,
+        toJSON: { virtuals: true }
+    }
+);
+
+
+const User = mongoose.model("User", userSchema)
+
+export default User;
