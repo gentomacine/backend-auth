1febf003a5e6eee09d64b1145b120740f4dfb16b
ogwalu chidozie <ogwaluchidozie@gmail.com>
2023-05-21 10:11:37 +0100

    logic to obtain the bearer token from header

---
+++ src/utils/obtaintokenfromheader.js
@@ -0,0 +1,13 @@
+
+export const obtainTokenFromHeader = req => {
+    const headersDetails = req.headers;
+    const token = headersDetails['authorization'].split(" ")[1]
+    if (token !== undefined) {
+        return token
+    } else {
+        return {
+            status: "error",
+            message: "it seems there is no token attached to the header"
+        }
+    }
+}
